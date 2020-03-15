fc6fe4d1cecc4501171b73fc704b17c42b11a01c
SuYxh <1806328384@qq.com>
2020-03-15 18:00:46 +0800

    pwa2

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,13 +1,28 @@
 module.exports = {
     title: 'ironc',
     description: 'ironc的文档, vuepress 文档',
-    base:"/vpblog/",
+    base: "/vpblog/",
+    head: [
+        ['link', { rel: 'icon', href: '/tg.png' }],
+        ['link', { rel: 'manifest', href: '/manifest.json' }]
+    ],
+    plugins: ['@vuepress/pwa', {
+        serviceWorker: true
+    }
+    ],
     themeConfig: {
-        repo: 'SuYxh/vpblog',  
+        repo: 'SuYxh/vpblog',
         editLinks: true,
         editLinkText: '编辑文档',
         docsDir: 'docs',
         lastUpdated: '最近更新',
+        serviceWorker:{
+            updatePopup:{
+                message:"内容已更新！",
+                buttonText:"刷新"
+            }
+
+        },
         nav: [
             { text: 'Home', link: '/' },
             { text: 'vuepress', link: '/vpstudy/' },
